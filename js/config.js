23a261c77f6e6f3f15328faccc9456ed6b6e7670
Adaptatech <gui98789@hotmail.com>
2024-09-21 17:15:11 -0400

    Add config.js + repartir
    Former-commit-id: 1b636682299c3da4e6e4123d22e5fafa789becb0

---
+++ js/config.js
@@ -0,0 +1,28 @@
+
+const spacePromptImages = [
+    { src: "../../images/switch-rouge.png", alt: "switch rouge" },
+    { src: "../../images/switch-bleue.png", alt: "switch bleue" },
+    { src: "../../images/big-mack.png", alt: "big mack" },
+    { src: "../../images/interact-switch.png", alt: "interact switch" },
+    { src: "../../images/switch-adaptatech.png", alt: "switch adaptatech" },
+    { src: "../../images/hand.png", alt: "hand" }
+];
+
+const spacePromptSounds = [
+    { value: "gong-sound", label: "Gong", src: "../../sounds/gong.mp3" },
+    { value: "piano-sound", label: "Piano", src: "../../sounds/piano.mp3" },
+    { value: "rooster-sound", label: "Coq", src: "../../sounds/rooster.mp3" },
+    { value: "record-own", label: "Enregistrez le vôtre" }
+];
+
+const visualOptions = [
+    { value: "normal", label: "Image normale" },
+    { value: "green-filter", label: "Saturation verte" },
+    { value: "red-filter", label: "Saturation rouge" },
+    { value: "blue-filter", label: "Saturation bleue" },
+    { value: "high-contrast", label: "Contraste élevé" },
+    { value: "grayscale", label: "Noir et blanc" },
+    { value: "invert", label: "Couleurs inversées" },
+    { value: "brightness", label: "Haute clarté" },
+    { value: "saturation", label: "Haute saturation" }
+];
