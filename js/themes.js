62688448214544ddd797da0609aead7e53eb3b3f
Chennizer <gui98789@hotmail.com>
2025-02-02 14:20:05 -0500

    test

---
+++ js/themes.js
@@ -0,0 +1,70 @@
+/* themes.js */
+window.themes = {
+    moana: {
+      images: [
+        "../../images/moana-background.jpg",
+        "../../images/noel.jpg",
+        "../../images/switch.webp"
+      ],
+      transparentPNGs: [
+        "../../images/moana.png",
+        "../../images/maui.png",
+        "../../images/pua.png",
+        "../../images/heihei.png",
+        "../../images/palmier.png"
+      ],
+      reinforcerVideos: [
+        "../../videos/asia.mp4",
+       
+      ],
+      words: [
+        "Moana", "noel","g","h"
+      ],
+      startSound: "../../sounds/firework.mp3",
+      reinforcerSound: "../../sounds/firework.mp3"
+    },
+    monde: {
+      images: [
+        "../../images/worldmap.jpg",
+        "../../images/wingsuit.png"
+      ],
+      transparentPNGs: [
+        "../../images/worldTrans1.png"
+      ],
+      reinforcerVideos: [
+        "../../videos/asia.mp4"
+      ],
+      words: [
+        "Monde", "Wingsuit"
+      ],
+      startSound: "../../sounds/monde-start.mp3"
+    },
+    nature: {
+      images: [
+        "../../images/nature-background1.png",
+        "../../images/nature-background2.png"
+      ],
+      transparentPNGs: [
+        "../../images/nature-leaf.png"
+      ],
+      reinforcerVideos: [
+        "../../videos/nature-reinforcer1.mp4"
+      ],
+      words: [
+        "Tree", "River", "Forest"
+      ],
+      startSound: "../../sounds/nature-start.mp3"
+    },
+    default: {
+      images: [
+        "../../images/toupieetbinou.png"
+      ],
+      transparentPNGs: [],
+      reinforcerVideos: [
+        "../../videos/toupieetbinouecole.mp4"
+      ],
+      words: [],
+      startSound: "../../sounds/default-start.mp3"
+    }
+  };
+  
