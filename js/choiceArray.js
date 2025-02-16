50e6e3823d1567015758e8ead0bc77c17ace621d
Chennizer <gui98789@hotmail.com>
2025-02-16 18:13:46 -0500

    choices

---
+++ js/choiceArray.js
@@ -0,0 +1,32 @@
+const mediaChoices = [
+    {
+      name: "Taylor Swift",
+      image: "../../images/taylorswift.jpg",    // path to tile image
+      video: "../../videos/tsshakeitoff.mp4",
+      audio: "../../sounds/taylorswift.mp3"       // path to associated video
+    },
+    {
+      name: "Moana",
+      image: "../../images/moana-background.jpg",    // path to tile image
+      video: "../../videos/moana-le-bleu-lumiere.mp4",
+      audio: "../../sounds/moana.mp3"
+    },
+    {
+      name: "Titouni",
+      image: "../../images/titounis.jpg",    // path to tile image
+      video: "../../videos/titouni-lescouleurs.mp4",
+      audio: "../../sounds/titouni.mp3" 
+    },
+    {
+      name: "Rick Astley",
+      image: "../../images/rickrolled.jpg",    // path to tile image
+      video: "../../videos/rickrolled.mp4",
+    },
+    {
+      name: "Lady Gaga",
+      image: "../../images/ladygaga.jpg",    // path to tile image
+      video: "../../videos/ladygagabadromance.mp4",
+    },
+
+  ];
+  
