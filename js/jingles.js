004644e40de142f8ab5189770e7784cc89cb399c
Chennizer <gui98789@hotmail.com>
2025-03-07 08:41:08 -0500

    test bucket + analytics
    Former-commit-id: 651965026ae1951e6d97d5266ff39fc9b4909669

---
+++ js/jingles.js
@@ -0,0 +1,18 @@
+window.jingleLibraryArray = [
+    {
+      name: "1",
+      src: "../../sounds/jingles/funnysound1.mp3"
+    },
+    {
+      name: "2",
+      src: "../../sounds/jingles/funnysound2.mp3"
+    },
+    {
+      name: "3",
+      src: "../../sounds/jingles/funnysound3.mp3"
+    },
+    {
+      name: "4",
+      src: "../../sounds/jingles/funnysound4.mp3"
+    }
+]
