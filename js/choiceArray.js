b928befdcc89854066276516adc789db74a47221
Chennizer <gui98789@hotmail.com>
2025-03-07 10:33:50 -0500

    video uploads
    Former-commit-id: bf5cfa7cbd03fcf9dce33c18d55a11ab2bab6852

--- js/choiceArray.js
+++ js/choiceArray.js
@@ -2,173 +2,173 @@ const mediaChoices = [
   {
     name: "Taylor Swift - Shake it off",
     image: "../../images/taylorswift.jpg",
-    video: "../../videos/tsshakeitoff.mp4",
+    video: "https://bucket.adaptatech.org/tsshakeitoff.mp4",
     audio: "../../sounds/taylorswift.mp3",
     category: "pop"
   },
   {
     name: "Moana - Le bleu lumière",
     image: "../../images/moana-background.jpg",
-    video: "../../videos/moana-le-bleu-lumiere.mp4",
+    video: "https://bucket.adaptatech.org/moana-le-bleu-lumiere.mp4",
     audio: "../../sounds/moana.mp3",
     category: "disney"
   },
   {
     name: "Titouni - Les couleurs",
     image: "../../images/titounis.jpg",
-    video: "../../videos/titouni-lescouleurs.mp4",
+    video: "https://bucket.adaptatech.org/titouni-lescouleurs.mp4",
     audio: "../../sounds/titouni.mp3",
     category: "enfant"
   },
   {
     name: "Arthur l'aventurier - Les oiseaux",
     image: "../../images/arthur.jpg",
-    video: "../../videos/arthur-oiseaux.mp4",
+    video: "https://bucket.adaptatech.org/arthur-oiseaux.mp4",
     category: "enfant"
   },
   {
     name: "Rick Astley - Never gonna give you up",
     image: "../../images/rickrolled.jpg",
-    video: "../../videos/rickrolled.mp4",
+    video: "https://bucket.adaptatech.org/rickrolled.mp4",
     category: "pop"
   },
   {
     name: "Lady Gaga - Bad Romance",
     image: "../../images/ladygaga.jpg",
-    video: "../../videos/ladygagabadromance.mp4",
+    video: "https://bucket.adaptatech.org/ladygagabadromance.mp4",
     category: "pop"
   },
   {
     name: "Ariane Grande - Problems",
     image: "../../images/arianagrande.webp", 
-    video: "../../videos/arianagrandeproblems.mp4",
+    video: "https://bucket.adaptatech.org/arianagrandeproblems.mp4",
     category: "pop"
   },
   {
     name: "Lady Gaga - Poker Face",
     image: "../../images/ladygagapokerface.webp", 
-    video: "../../videos/ladygagapokerface.mp4",
+    video: "https://bucket.adaptatech.org/ladygagapokerface.mp4",
     category: "pop"
   },
   {
     name: "La belle et la bête - Belle",
     image: "../../images/belleetbete.jpg",
-    video: "../../videos/belleetbete-belle.mp4",
+    video: "https://bucket.adaptatech.org/belleetbete-belle.mp4",
     category: "disney"
   },
   {
     name: "Kids United - On écrit sur les murs",
     image: "../../images/kidsunited.jpg",
-    video: "../../videos/kidsunited-on-ecrit.mp4",
+    video: "https://bucket.adaptatech.org/kidsunited-on-ecrit.mp4",
     category: "enfant"
 
   },
   {
     name: "Comptine africaine - Tape des mains",
     image: "../../images/ca.jpg",
-    video: "../../videos/ca-tape-des-mains.mp4",
+    video: "https://bucket.adaptatech.org/ca-tape-des-mains.mp4",
     category: "enfant"
   },
   {
     name: "La reine des neiges - Libérée, délivrée",
     image: "../../images/frozen.jpg",
-    video: "../../videos/frozen-libéréedélivrée.mp4",
+    video: "https://bucket.adaptatech.org/frozen-libéréedélivrée.mp4",
     category: "disney"
   },
   {
     name: "Encanto - Ne parlons pas de Bruno",
     image: "../../images/encanto.jpg",
-    video: "../../videos/encanto-ne-parlons-pas-de-bruno.mp4",
+    video: "https://bucket.adaptatech.org/encanto-ne-parlons-pas-de-bruno.mp4",
     category: "disney"
   },
   {
     name: "Passe Partout - Introduction",
     image: "../../images/passe-partout-control-panel.webp",
-    video: "../../videos/passe-partout-intro.mp4",
+    video: "https://bucket.adaptatech.org/passe-partout-intro.mp4",
     category: "enfant"
   },
   {
     name: "Roi Lion - Histoire de la vie",
     image: "../../images/roi-lion.jpg",
-    video: "../../videos/roi-lion-histoire-de-la-vie.mp4",
+    video: "https://bucket.adaptatech.org/roi-lion-histoire-de-la-vie.mp4",
     category: "disney"
   },
   {
     name: "TFO - bonjour",
     image: "../../images/tfo-saisons.jpg",
-    video: "../../videos/tfo-bonjour.mp4",
+    video: "https://bucket.adaptatech.org/tfo-bonjour.mp4",
     category: ["enfant","bonjour"]
   },
   {
     name: "Kendrick Lamar - All the stars",
     image: "../../images/kendricklamar.webp",
-    video: "../../videos/kendricklamar-allthestars.mp4",
+    video: "https://bucket.adaptatech.org/kendricklamar-allthestars.mp4",
     category: "hip hop"
   },
   {
     name: "Post Malone - Sunflower",
     image: "../../images/postmalone-sunflower.jpg",
-    video: "../../videos/postmalone-sunflower.mp4",
+    video: "https://bucket.adaptatech.org/postmalone-sunflower.mp4",
     category: "hip hop"
   },
   {
     name: "Eminem - Lose Yourself",
     image: "../../images/eminem-loseyourself.webp",
-    video: "../../videos/eminem-loseyourself.mp4",
+    video: "https://bucket.adaptatech.org/eminem-loseyourself.mp4",
     category: "hip hop"
   },
   {
     name: "Wu Tang Clan - The mystery of chessboxin",
     image: "../../images/wutangclan.jpg",
-    video: "../../videos/wutang-chessboxin.mp4",
+    video: "https://bucket.adaptatech.org/wutang-chessboxin.mp4",
     category: "hip hop"
   },
   {
     name: "Macklemore - Thrift Shop",
     image: "../../images/macklemore-thriftshop.jpg",
-    video: "../../videos/macklemore-thriftshop.mp4",
+    video: "https://bucket.adaptatech.org/macklemore-thriftshop.mp4",
     category: "hip hop"
   },
   {
     name: "Sum 41 - Landmines",
     image: "../../images/sum41.jpg",
-    video: "../../videos/sum41-landmines.mp4",
+    video: "https://bucket.adaptatech.org/sum41-landmines.mp4",
     category: "rock"
   },
   {
     name: "Green Day - Basketcase",
     image: "../../images/greenday-basketcase.jpg",
-    video: "../../videos/greenday-basketcase.mp4",
+    video: "https://bucket.adaptatech.org/greenday-basketcase.mp4",
     category: "rock"
   },
   {
     name: "Cage The Elephant - Come a little closer",
     image: "../../images/cagetheelephant-comealittlecloser.jpg",
-    video: "../../videos/cagetheelephant-comealittlecloser.mp4",
+    video: "https://bucket.adaptatech.org/cagetheelephant-comealittlecloser.mp4",
     category: "rock"
   },
   {
     name: "Linkin Park - In the end",
     image: "../../images/linkinpark.webp",
-    video: "../../videos/linkinpark-intheend.mp4",
+    video: "https://bucket.adaptatech.org/linkinpark-intheend.mp4",
     category: "rock"
   },
   {
     name: "Alain le Lait - Bonjour",
     image: "../../images/alainlelait-bonjour.jpg",
-    video: "../../videos/alainlelait-bonjour.mp4",
+    video: "https://bucket.adaptatech.org/alainlelait-bonjour.mp4",
     category: ["enfant","bonjour"]
   },
   {
     name: "Déménageurs - Bonjour",
     image: "../../images/demenageurs-bonjour.jpg",
-    video: "../../videos/demenageurs-bonjour.mp4",
+    video: "https://bucket.adaptatech.org/demenageurs-bonjour.mp4",
     category: ["enfant","bonjour"]
   },
   {
     name: "All American Rejects - Dirty Little Secret",
     image: "../../images/allamericanrejects-dirtylittlesecret.jpg",   
-    video: "../../videos/allamericanrejects-dirtylittlesecret.mp4",
+    video: "https://bucket.adaptatech.org/allamericanrejects-dirtylittlesecret.mp4",
     category: ["rock"]
   },
 
