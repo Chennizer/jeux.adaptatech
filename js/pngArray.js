651965026ae1951e6d97d5266ff39fc9b4909669
Chennizer <gui98789@hotmail.com>
2025-03-07 08:41:08 -0500

    test bucket + analytics

---
+++ js/pngArray.js
@@ -0,0 +1,48 @@
+window.imageLibraryArray = [
+    {
+      name: "Tigre",
+      src: "../../images/cartoontiger.png"
+    },
+    {
+      name: "Lion",
+      src: "../../images/cartoonlion.png"
+    },
+    {
+      name: "Singe",
+      src: "../../images/cartoongorilla.png"
+    },
+    {
+      name: "Eléphant",
+      src: "../../images/cartoonelephant.png"
+    },
+    {
+      name: "Rhino",
+      src: "../../images/cartoonrhino.png"
+    },
+    {
+      name: "Giraffe",
+      src: "../../images/cartoongiraffe.png"
+    },
+    {
+      name: "Astronaute",
+      src: "../../images/astronaut.png"
+    },
+    {
+      name: "Yellow submarine",
+      src: "../../images/beatles.png"
+    },
+    {
+      name: "Lion",
+      src: "../../images/cartoonlion.png"
+    },
+    {
+      name: "Singe",
+      src: "../../images/cartoongorilla.png"
+    },
+    {
+      name: "Astronaute",
+      src: "../../images/astronaut.png"
+    },
+
+  ];
+  
