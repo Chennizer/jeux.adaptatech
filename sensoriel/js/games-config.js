65f69de992cc62ff64f289b659126ddbfc882ea1
Chennizer <95369342+Chennizer@users.noreply.github.com>
2025-10-08 10:34:07 -0400

    Rework Match Number into addition composition game

--- sensoriel/js/games-config.js
+++ sensoriel/js/games-config.js
@@ -66,6 +66,19 @@
           min: 1,
           max: 10,
           value: 1
+        },
+        {
+          type: 'number',
+          label: 'Maximum addend:',
+          name: 'maxAddend',
+          min: 2,
+          max: 10,
+          value: 5
+        },
+        {
+          type: 'checkbox',
+          label: 'Afficher le cadre de dix:',
+          name: 'showTenFrame'
         }
       ]
     },
